adab1143462bca3374aa13a7427f094e66551a03
gerritsxd <gerrit.sevilla@gmail.com>
2022-01-07 10:33:33 +0100

    deployment test

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -98,7 +98,7 @@
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                        {{ __('Log me out') }}
+                                        {{ __('Log for out') }}
                                     </a>
 
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
