8dfd4b4bae174296670b356e5d9c989639876a3c
gerritsxd <gerrit.sevilla@gmail.com>
2022-01-20 18:38:46 +0100

    Invitation cicle complete bugfix

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -7,6 +7,7 @@ use App\Models\InvitationCode;
 use App\Providers\RouteServiceProvider;
 use App\Models\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Str;
